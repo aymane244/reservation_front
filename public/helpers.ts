e22448117267465f593ff3f5178b11cb447a8050
unknown <a.chnaif2010@gmail.com>
2025-08-31 22:24:59 +0100

    fix bugs

--- public/helpers.ts
+++ public/helpers.ts
@@ -1,4 +1,5 @@
-export const url = window.location.hostname === "localhost" ? "http://localhost:8000" : "https://reservation.aimane-web-dev.com/";
+export const url = window.location.hostname === "localhost" ? "http://localhost:8000" : "https://reservation.aimane-web-dev.com";
+console.log(url);
 
 export const firstLetterCapital = (text) =>{
     if (!text) return '';
